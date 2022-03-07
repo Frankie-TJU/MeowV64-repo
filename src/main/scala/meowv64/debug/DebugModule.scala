f8d233969b9d4ee472abaf4e52293667543ffddd
Jiajie Chen <c@jia.je>
2022-03-08 01:25:51 +0800

    Initial abstract command

--- src/main/scala/meowv64/debug/DebugModule.scala
+++ src/main/scala/meowv64/debug/DebugModule.scala
@@ -66,6 +66,29 @@ class DMStatus extends Bundle {
   val version = UInt(4.W)
 }
 
+class HartInfo extends Bundle {
+  val nscratch = UInt(4.W)
+  val zero = UInt(3.W)
+  val dataaccess = Bool()
+  val datasize = UInt(4.W)
+  val dataaddr = UInt(4.W)
+}
+
+class AbstractCS extends Bundle {
+  val progbufsize = UInt(5.W)
+  val zero1 = UInt(11.W)
+  val busy = Bool()
+  val relaxedpriv = Bool()
+  val cmderr = UInt(3.W)
+  val zero2 = UInt(4.W)
+  val datacount = UInt(4.W)
+}
+
+class AbstractCmd extends Bundle {
+  val cmdtype = UInt(8.W)
+  val control = UInt(24.W)
+}
+
 class DebugModule(implicit sDef: SystemDef) extends Module {
   val io = IO(new Bundle {
     val dmi = new DebugModuleInterface()
@@ -78,6 +101,14 @@ class DebugModule(implicit sDef: SystemDef) extends Module {
   val curReq = Reg(new DebugModuleReq())
   val curResp = Reg(new DebugModuleResp())
   io.dmi.resp.bits := curResp
+
+  // abstract data
+  val absData = RegInit(VecInit.fill(12)(0.U(32.W)))
+
+  // abstractcs registers
+  val absBusy = RegInit(false.B)
+  val relaxedpriv = RegInit(false.B)
+  val cmderr = RegInit(0.U(3.W))
 
   // dmcontrol registers
   val hartreset = RegInit(false.B)
@@ -96,7 +127,23 @@ class DebugModule(implicit sDef: SystemDef) extends Module {
       }
     }
     is(DebugModuleState.req) {
+      // default: fail
+      done := true.B
+      curResp.fail := true.B
+
       switch(curReq.address) {
+        is(0x04.U, 0x05.U, 0x06.U, 0x07.U, 0x08.U, 0x09.U, 0x0a.U, 0x0b.U, 0x0c.U, 0x0d.U, 0x0e.U, 0x0f.U) {
+          // data0~data11
+          val idx = curReq.address - 0x04.U
+          when(curReq.isRead) {
+            curResp.data := absData(idx)
+          }.otherwise {
+            absData(idx) := curReq.data
+          }
+
+          curResp.fail := false.B
+          done := true.B
+        }
         is(0x10.U) {
           // dmcontrol
           when(curReq.isRead) {
@@ -111,7 +158,6 @@ class DebugModule(implicit sDef: SystemDef) extends Module {
             resp.dmactive := dmactive
 
             curResp.data := resp.asUInt
-            curResp.fail := false.B
           }.otherwise {
             val req = Wire(new DMControl)
             req := curReq.data.asTypeOf(req)
@@ -122,17 +168,62 @@ class DebugModule(implicit sDef: SystemDef) extends Module {
             ndmreset := req.ndmreset
             dmactive := req.dmactive
           }
+          curResp.fail := false.B
           done := true.B
         }
         is(0x11.U) {
           // dmstatus
           when(curReq.isRead) {
             val resp = WireInit(0.U.asTypeOf(new DMStatus))
+            resp.anynonexistent := hartsel >= sDef.CORE_COUNT.U
             resp.version := 3.U // debug 1.0
 
             curResp.data := resp.asUInt
-            curResp.fail := false.B
           }
+          curResp.fail := false.B
+          done := true.B
+        }
+        is(0x12.U) {
+          // hartinfo
+          when(curReq.isRead) {
+            val resp = WireInit(0.U.asTypeOf(new HartInfo))
+
+            curResp.data := resp.asUInt
+          }
+          curResp.fail := false.B
+          done := true.B
+        }
+        is(0x16.U) {
+          // abstractcs
+          when(curReq.isRead) {
+            val resp = WireInit(0.U.asTypeOf(new AbstractCS))
+            resp.busy := absBusy
+            resp.relaxedpriv := relaxedpriv
+            resp.cmderr := cmderr
+            resp.datacount := 12.U
+
+            curResp.data := resp.asUInt
+          }.otherwise {
+            val req = Wire(new AbstractCS)
+            req := curReq.data.asTypeOf(req)
+
+            relaxedpriv := req.relaxedpriv
+            when(req.cmderr =/= 0.U) {
+              cmderr := 0.U
+            }
+          }
+          curResp.fail := false.B
+          done := true.B
+        }
+        is(0x17.U) {
+          // command
+          when(curReq.isRead) {
+            curResp.data := 0.U
+          }.otherwise {
+            val req = Wire(new AbstractCmd)
+            req := curReq.data.asTypeOf(req)
+          }
+          curResp.fail := false.B
           done := true.B
         }
       }
