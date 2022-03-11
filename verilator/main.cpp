a15d18556298b4c495a4cc57ff2b08cd7a9273c3
Jiajie Chen <c@jia.je>
2022-03-11 09:23:16 +0800

    Initial jtag vpi server

--- verilator/main.cpp
+++ verilator/main.cpp
@@ -32,6 +32,34 @@ bool jtag = false;
 bool jtag_rbb = false;
 // use jtag_vpi protocol
 bool jtag_vpi = false;
+
+// jtag_vpi state & definitions
+enum JtagVpiState {
+  CHECK_CMD,
+  TAP_RESET,
+  GOTO_IDLE,
+  DO_TMS_SEQ,
+  SCAN_CHAIN,
+  FINISHED
+} jtag_vpi_state;
+bool jtag_vpi_tms_flip = false;
+
+enum JtagVpiCommand {
+  CMD_RESET,
+  CMD_TMS_SEQ,
+  CMD_SCAN_CHAIN,
+  CMD_SCAN_CHAIN_FLIP_TMS,
+  CMD_STOP_SIMU
+};
+
+struct jtag_vpi_cmd {
+  uint32_t cmd;
+  uint8_t buffer_out[512];
+  uint8_t buffer_in[512];
+  uint32_t length;
+  uint32_t nb_bits;
+};
+
 int res = 0;
 
 const uint64_t AXI_DATA_WIDTH = 128;
@@ -434,7 +462,7 @@ int jtag_rbb_init() {
   return 0;
 }
 
-void jtag_rbb_tick(VRiscVSystem *top) {
+void jtag_rbb_tick() {
   if (client_fd >= 0) {
     static char read_buffer[128];
     static size_t read_buffer_count = 0;
@@ -500,6 +528,151 @@ void jtag_rbb_tick(VRiscVSystem *top) {
   }
 }
 
+int jtag_vpi_init() {
+  listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_fd < 0) {
+    perror("socket");
+    return -1;
+  }
+
+  // set non blocking
+  fcntl(listen_fd, F_SETFL, O_NONBLOCK);
+
+  int reuseaddr = 1;
+  if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) <
+      0) {
+    perror("setsockopt");
+    return -1;
+  }
+
+  int port = 12345;
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(port);
+
+  if (bind(listen_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror("bind");
+    return -1;
+  }
+
+  if (listen(listen_fd, 1) == -1) {
+    perror("listen");
+    return -1;
+  }
+  fprintf(stderr, "> JTAG vpi server listening at :12345\n");
+
+  jtag_vpi_state = JtagVpiState::CHECK_CMD;
+
+  return 0;
+}
+
+bool write_socket_full(int fd, uint8_t *data, size_t count) {
+  size_t sent = 0;
+  while (sent < count) {
+    ssize_t count = write(fd, &data[sent], count - sent);
+    if (count > 0) {
+      sent += count;
+    } else if (count < 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void jtag_vpi_tick() {
+  // ref jtag_vpi project jtagServer.cpp
+
+  static uint8_t jtag_vpi_buffer[sizeof(struct jtag_vpi_cmd)];
+  static size_t jtag_vpi_recv = 0;
+  // automatic tck clock
+  static size_t tck_counter = 0;
+  tck_counter++;
+  static bool tck_en = false;
+  top->io_jtag_tck = tck_en ? (tck_counter % 2) : 0;
+
+  struct jtag_vpi_cmd *cmd = (struct jtag_vpi_cmd *)jtag_vpi_buffer;
+
+  if (jtag_vpi_state == JtagVpiState::CHECK_CMD) {
+    if (client_fd >= 0) {
+      ssize_t num_read = read(client_fd, &jtag_vpi_buffer[jtag_vpi_recv],
+                              sizeof(jtag_vpi_cmd) - jtag_vpi_recv);
+      if (num_read > 0) {
+        jtag_vpi_recv += num_read;
+      } else if (num_read == 0) {
+        // remote socket closed
+        fprintf(stderr, "> JTAG debugger detached\n");
+        close(client_fd);
+        client_fd = -1;
+      }
+
+      if (jtag_vpi_recv == sizeof(struct jtag_vpi_cmd)) {
+        // cmd valid
+        jtag_vpi_recv = 0;
+        switch (cmd->cmd) {
+        case CMD_RESET:
+          jtag_vpi_state = TAP_RESET;
+          break;
+        case CMD_TMS_SEQ:
+          jtag_vpi_state = DO_TMS_SEQ;
+          break;
+        case CMD_SCAN_CHAIN:
+          jtag_vpi_state = SCAN_CHAIN;
+          jtag_vpi_tms_flip = false;
+          break;
+        case CMD_SCAN_CHAIN_FLIP_TMS:
+          jtag_vpi_state = SCAN_CHAIN;
+          jtag_vpi_tms_flip = true;
+          break;
+        }
+      }
+    } else {
+      client_fd = accept(listen_fd, NULL, NULL);
+      if (client_fd > 0) {
+        fcntl(client_fd, F_SETFL, O_NONBLOCK);
+
+        // set nodelay
+        int flags = 1;
+        if (setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, (void *)&flags,
+                       sizeof(flags)) < 0) {
+          perror("setsockopt");
+        }
+        fprintf(stderr, "> JTAG debugger attached\n");
+      }
+    }
+  } else if (jtag_vpi_state == JtagVpiState::TAP_RESET) {
+    // tap reset
+    // tms=1 for five tck clocks
+    static int reset_counter = 0;
+    if (tck_counter % 2 == 0) {
+      // tck fall
+      top->io_jtag_tms = 1;
+      tck_en = true;
+    } else if (tck_en) {
+      // tck rise
+      reset_counter++;
+      if (reset_counter >= 5) {
+        reset_counter = 0;
+        tck_en = false;
+        jtag_vpi_state = JtagVpiState::GOTO_IDLE;
+      }
+    }
+  } else if (jtag_vpi_state == JtagVpiState::GOTO_IDLE) {
+    // tap go to idle
+    // tms=0 for one tck clock
+    if (tck_counter % 2 == 0) {
+      // tck fall
+      top->io_jtag_tms = 0;
+      tck_en = true;
+    } else if (tck_en) {
+      // tck rise
+      tck_en = false;
+      jtag_vpi_state = JtagVpiState::CHECK_CMD;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   Verilated::commandArgs(argc, argv);
 
@@ -541,9 +714,12 @@ int main(int argc, char **argv) {
     if (jtag_rbb && jtag_rbb_init() < 0) {
       return -1;
     }
+    if (jtag_vpi && jtag_vpi_init() < 0) {
+      return -1;
+    }
 
     // init
-    top->io_jtag_tck = 1;
+    top->io_jtag_tck = 0;
     top->io_jtag_tms = 1;
     top->io_jtag_tdi = 1;
     top->io_jtag_trstn = 1;
@@ -627,7 +803,10 @@ int main(int argc, char **argv) {
     if (jtag && (main_time % 10) == 0) {
       // jtag tick
       if (jtag_rbb) {
-        jtag_rbb_tick(top);
+        jtag_rbb_tick();
+      }
+      if (jtag_vpi) {
+        jtag_vpi_tick();
       }
     }
   }
