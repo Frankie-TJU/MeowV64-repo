e3a77e8df6cf29d4ad48204e8ebeb1ea196f47c6
Jiajie Chen <c@jia.je>
2022-04-18 09:57:33 +0800

    Update benchmark result

--- verilator/benchmark.sh
+++ verilator/benchmark.sh
@@ -4,5 +4,5 @@ set -e
 make
 echo -n > benchmark.log
 for filename in ../../testcases/riscv-tests/build/benchmarks/*.riscv; do
-	./VRiscVSystem $filename | tee -a benchmark.log
+	./VRiscVSystem $filename 2>&1 | tee -a benchmark.log
 done
