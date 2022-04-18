5a72286c2b2eb451742b64e5c2f89ecd2edb34fb
Jiajie Chen <c@jia.je>
2022-04-18 10:22:56 +0800

    Simplify test.sh

--- testcases/test.sh
+++ testcases/test.sh
@@ -5,13 +5,9 @@ make
 for VLEN in 128 256 512 1024
 do
 	SPIKE="spike --varch=vlen:${VLEN},elen:64 --isa=rv64gcv -m0x00000000:0x40000000,0x60000000:0x1000,0x80000000:0x10000000"
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vle
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vse
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vluxei
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vfmv
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vsll
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vfadd
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vmvr
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vfmacc
-	$SPIKE ./riscv-tests/build/isa/rv64uv-p-vfredosum
+	for TEST in vle vse vluxei vfmv vsll vfadd vmvr vfmacc vfredosum
+	do
+		$SPIKE ./riscv-tests/build/isa/rv64uv-p-${TEST}
+	done
+	$SPIKE ./riscv-tests/build/benchmarks/spmv.riscv
 done
