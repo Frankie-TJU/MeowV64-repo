d904fb2906a6672e2e4b4055486e94228292ff26
Jiajie Chen <c@jia.je>
2022-05-06 13:08:13 +0800

    Add more fma family tests

--- testcases/test.sh
+++ testcases/test.sh
@@ -5,7 +5,28 @@ make
 for VLEN in 128 256 512 1024
 do
 	SPIKE="spike --varch=vlen:${VLEN},elen:64 --isa=rv64gcv -m0x00000000:0x40000000,0x60000000:0x1000,0x80000000:0x10000000"
-	for TEST in vadd vfadd vfmacc vfmadd vfmsub vfmul vfmv vfredosum vle vluxei vmv vmvr vse vsetvl vsll
+	for TEST in \
+		vadd \
+		vfadd \
+		vfmacc \
+		vfmadd \
+		vfmsac \
+		vfmsub \
+		vfmul \
+		vfmv \
+		vfnmacc \
+		vfnmadd \
+		vfnmsac \
+		vfnmsub \
+		vfredosum \
+		vfsub \
+		vle \
+		vluxei \
+		vmv \
+		vmvr \
+		vse \
+		vsetvl \
+		vsll
 	do
 		$SPIKE ./riscv-tests/build/isa/rv64uv-p-${TEST}
 	done
