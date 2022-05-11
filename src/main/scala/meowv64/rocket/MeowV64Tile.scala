50a4d9bc9f47060febc0009899f566ccecd6a1f6
Jiajie Chen <c@jia.je>
2022-05-11 23:14:55 +0800

    Initial rocket tile

---
+++ src/main/scala/meowv64/rocket/MeowV64Tile.scala
@@ -0,0 +1,198 @@
+package meowv64.rocket
+
+import chisel3._
+import freechips.rocketchip.config._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.rocket._
+import freechips.rocketchip.subsystem.{RocketCrossingParams}
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.interrupts._
+import freechips.rocketchip.util._
+import freechips.rocketchip.tile._
+import freechips.rocketchip.prci.ClockSinkParameters
+import meowv64.core.CoreDef
+import meowv64.system.SystemDef
+import meowv64.core.Core
+import meowv64.system.DefaultSystemDef
+
+case class MeowV64CoreParams(
+    coredef: CoreDef
+) extends CoreParams {
+  /* DO NOT CHANGE BELOW THIS */
+  val bootFreqHz: BigInt = 0
+  val useVM: Boolean = true
+  val useHypervisor: Boolean = false
+  val useUser: Boolean = true
+  val useSupervisor: Boolean = false
+  val useDebug: Boolean = true
+  val useAtomics: Boolean = true
+  val useAtomicsOnlyForIO: Boolean = false
+  val useCompressed: Boolean = true
+  override val useBitManip: Boolean = true
+  override val useVector: Boolean = true
+  val useSCIE: Boolean = false
+  val useRVE: Boolean = false
+  val mulDiv: Option[MulDivParams] = Some(MulDivParams())
+  val fpu: Option[FPUParams] = Some(FPUParams())
+  val fetchWidth: Int = 1
+  val decodeWidth: Int = 1
+  val retireWidth: Int = 2
+  val instBits: Int = if (useCompressed) 16 else 32
+  val nLocalInterrupts: Int = 0
+  val useNMI: Boolean = false
+  val nPMPs: Int = 0
+  val pmpGranularity: Int = 4
+  val nBreakpoints: Int = 0
+  val useBPWatch: Boolean = false
+  val mcontextWidth: Int = 0
+  val scontextWidth: Int = 0
+  val nPerfCounters: Int = 0
+  val haveBasicCounters: Boolean = true
+  val haveFSDirty: Boolean = false
+  val misaWritable: Boolean = false
+  val haveCFlush: Boolean = false
+  val nL2TLBEntries: Int = 512
+  val nL2TLBWays: Int = 1
+  val nPTECacheEntries: Int = 8 // TODO: Check
+  val mtvecInit: Option[BigInt] = Some(BigInt(0))
+  val mtvecWritable: Boolean = true
+
+  def lrscCycles: Int = 80
+}
+
+case class MeowV64TileAttachParams(
+    tileParams: MeowV64TileParams,
+    crossingParams: RocketCrossingParams
+) extends CanAttachTile {
+  type TileType = MeowV64Tile
+  val lookup = PriorityMuxHartIdFromSeq(Seq(tileParams))
+}
+
+case class MeowV64TileParams(
+    name: Option[String] = Some("meowv64_tile"),
+    hartId: Int = 0,
+    systemDef: SystemDef = new DefaultSystemDef
+) extends InstantiableTileParams[MeowV64Tile] {
+  val core: MeowV64CoreParams = MeowV64CoreParams(
+    CoreDef.default(hartId, systemDef.INIT_VEC, systemDef.L2_LINE_BYTES)
+  )
+  val icache: Option[ICacheParams] = Some(ICacheParams())
+  val dcache: Option[DCacheParams] = Some(DCacheParams())
+  val btb: Option[BTBParams] = Some(BTBParams())
+  val beuAddr: Option[BigInt] = None
+  val blockerCtrlAddr: Option[BigInt] = None
+  val clockSinkParams: ClockSinkParameters = ClockSinkParameters()
+
+  def instantiate(crossing: TileCrossingParamsLike, lookup: LookupByHartIdImpl)(
+      implicit p: Parameters
+  ): MeowV64Tile = {
+    new MeowV64Tile(this, crossing, lookup)
+  }
+}
+
+class MeowV64Tile private (
+    val meowv64Params: MeowV64TileParams,
+    crossing: ClockCrossingType,
+    lookup: LookupByHartIdImpl,
+    q: Parameters
+) extends BaseTile(meowv64Params, crossing, lookup, q)
+    with SinksExternalInterrupts
+    with SourcesExternalNotifications {
+
+  /** Setup parameters: Private constructor ensures altered LazyModule.p is used
+    * implicitly
+    */
+  def this(
+      params: MeowV64TileParams,
+      crossing: TileCrossingParamsLike,
+      lookup: LookupByHartIdImpl
+  )(implicit p: Parameters) =
+    this(params, crossing.crossingType, lookup, p)
+
+  val intOutwardNode = IntIdentityNode()
+  val slaveNode = TLIdentityNode()
+  val masterNode = visibilityNode
+
+  tlOtherMastersNode := tlMasterXbar.node
+  masterNode :=* tlOtherMastersNode
+  DisableMonitors { implicit p => tlSlaveXbar.node :*= slaveNode }
+
+  val cpuDevice: SimpleDevice =
+    new SimpleDevice("cpu", Seq("meow,meowv64", "riscv")) {
+      override def parent = Some(ResourceAnchors.cpus)
+      override def describe(resources: ResourceBindings): Description = {
+        val Description(name, mapping) = super.describe(resources)
+        Description(
+          name,
+          mapping ++
+            cpuProperties ++
+            nextLevelCacheProperty ++
+            tileProperties
+        )
+      }
+    }
+
+  ResourceBinding {
+    Resource(cpuDevice, "reg").bind(ResourceAddress(staticIdForMetadataUseOnly))
+  }
+
+  override lazy val module = new MeowV64TileModuleImp(this)
+
+  /** Setup AXI4 memory interface. THESE ARE CONSTANTS.
+    */
+  val portName = "meowv64-mem-port"
+  val node = TLIdentityNode()
+
+  val dmemNode = TLClientNode(
+    Seq(
+      TLMasterPortParameters.v1(
+        clients =
+          Seq(TLMasterParameters.v1(name = portName, sourceId = IdRange(0, 1)))
+      )
+    )
+  )
+
+  val imemNode = TLClientNode(
+    Seq(
+      TLMasterPortParameters.v1(
+        clients =
+          Seq(TLMasterParameters.v1(name = portName, sourceId = IdRange(0, 1)))
+      )
+    )
+  )
+
+  tlMasterXbar.node := node := TLBuffer() := dmemNode
+  tlMasterXbar.node := node := TLBuffer() := imemNode
+
+  def connectMeowV64Interrupts(
+      meip: Bool,
+      seip: Bool,
+      mtip: Bool,
+      msip: Bool
+  ) {
+    val (interrupts, _) = intSinkNode.in(0)
+    meip := interrupts(0)
+    seip := interrupts(1)
+    mtip := interrupts(2)
+    msip := interrupts(3)
+  }
+}
+
+class MeowV64TileModuleImp(outer: MeowV64Tile)
+    extends BaseTileModuleImp(outer) {
+  // annotate the parameters
+  Annotated.params(this, outer.meowv64Params)
+
+  // connect the meowv64 core
+  val core = Module(
+    new Core()(outer.meowv64Params.core.coredef)
+  )
+
+  outer.connectMeowV64Interrupts(
+    core.io.int.meip,
+    core.io.int.seip,
+    core.io.int.mtip,
+    core.io.int.msip
+  )
+}
