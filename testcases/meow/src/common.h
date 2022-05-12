9b29ff4847a984a6ddce51a2816956ebf33524f7
Jiajie Chen <c@jia.je>
2022-05-12 17:45:00 +0800

    Use tohost in meow tests and cleanup mem and mmio

--- testcases/meow/src/common.h
+++ testcases/meow/src/common.h
@@ -7,6 +7,12 @@ tohost: .dword 0
 fromhost: .dword 0
 
 #define FAIL 2: \
+  li a0, 3; \
+  la a1, tohost; \
+  sw a0, 0(a1); \
+  j 2b;
+
+#define SUCCESS 2: \
   li a0, 1; \
   la a1, tohost; \
   sw a0, 0(a1); \
--- verilator/rocket/main.cpp
+++ verilator/rocket/main.cpp
@@ -116,18 +116,14 @@ void step_mem() {
     top->mem_axi4_RVALID = 1;
     top->mem_axi4_RID = pending_read_id;
     mpz_class r_data;
-    if (pending_read_addr == 0x10001014) {
-      // serial lsr
-      r_data = 1L << (32 + 5);
-    } else {
-      uint64_t aligned = (pending_read_addr / AXI_DATA_BYTES) * AXI_DATA_BYTES;
-      for (int i = 0; i < AXI_DATA_BYTES / sizeof(mem_t); i++) {
-        uint64_t addr = aligned + i * sizeof(mem_t);
-        mem_t r = memory[addr];
-        mpz_class res = r;
-        res <<= (i * (sizeof(mem_t) * 8));
-        r_data += res;
-      }
+
+    uint64_t aligned = (pending_read_addr / AXI_DATA_BYTES) * AXI_DATA_BYTES;
+    for (int i = 0; i < AXI_DATA_BYTES / sizeof(mem_t); i++) {
+      uint64_t addr = aligned + i * sizeof(mem_t);
+      mem_t r = memory[addr];
+      mpz_class res = r;
+      res <<= (i * (sizeof(mem_t) * 8));
+      r_data += res;
     }
 
     mpz_class mask = 1;
@@ -235,41 +231,6 @@ void step_mem() {
       }
 
       uint64_t input = wdata.get_ui();
-      if (pending_write_addr == 0x10001000) {
-        // serial
-        printf("%c", input & 0xFF);
-        fflush(stdout);
-      } else if (pending_write_addr == tohost_addr) {
-        // tohost
-        uint32_t data = input & 0xFFFFFFFF;
-        if (input == ((data & 0xFF) | 0x0101000000000000L)) {
-          // serial
-          printf("%c", input & 0xFF);
-        } else if (data == 1) {
-          // pass
-          fprintf(stderr, "> ISA testsuite pass\n");
-          if (!jtag) {
-            finished = true;
-          }
-        } else if ((data & 1) == 1) {
-          uint32_t c = data >> 1;
-          fprintf(stderr, "> ISA testsuite failed case %d\n", c);
-          if (!jtag) {
-            finished = true;
-          }
-          res = 1;
-        } else {
-          fprintf(stderr, "> Unhandled tohost: %x\n", input);
-        }
-      } else if (pending_write_addr == fromhost_addr) {
-        // write to fromhost
-        // clear tohost
-        for (int i = 0; i < AXI_DATA_BYTES / sizeof(mem_t); i++) {
-          uint64_t addr = tohost_addr + i * sizeof(mem_t);
-          memory[addr] = 0;
-        }
-      }
-
       pending_write_addr += 1L << pending_write_size;
       pending_write_len--;
       if (top->mem_axi4_WLAST) {
@@ -313,15 +274,6 @@ void step_mmio() {
     if (pending_read_addr == 0x10001014) {
       // serial lsr
       r_data = 1L << (32 + 5);
-    } else {
-      uint64_t aligned = (pending_read_addr / AXI_DATA_BYTES) * AXI_DATA_BYTES;
-      for (int i = 0; i < AXI_DATA_BYTES / sizeof(mem_t); i++) {
-        uint64_t addr = aligned + i * sizeof(mem_t);
-        mem_t r = memory[addr];
-        mpz_class res = r;
-        res <<= (i * (sizeof(mem_t) * 8));
-        r_data += res;
-      }
     }
 
     mpz_class mask = 1;
@@ -394,39 +346,6 @@ void step_mmio() {
       mpz_class wdata;
       mpz_import(wdata.get_mpz_t(), AXI_DATA_BYTES / 4, -1, 4, -1, 0,
                  top->mmio_axi4_WDATA);
-
-      uint64_t aligned = pending_write_addr / AXI_DATA_BYTES * AXI_DATA_BYTES;
-      for (int i = 0; i < AXI_DATA_BYTES / sizeof(mem_t); i++) {
-        uint64_t addr = aligned + i * sizeof(mem_t);
-
-        mpz_class local_wdata_mpz = wdata >> (i * (sizeof(mem_t) * 8));
-        mem_t local_wdata = local_wdata_mpz.get_ui();
-
-        uint64_t local_wstrb =
-            (top->mmio_axi4_WSTRB >> (i * sizeof(mem_t))) & 0xfL;
-
-        mpz_class local_mask_mpz = shifted_mask >> (i * sizeof(mem_t));
-        uint64_t local_mask = local_mask_mpz.get_ui() & 0xfL;
-        if (local_mask & local_wstrb) {
-          mem_t base = memory[addr];
-          mem_t input = local_wdata;
-          uint64_t be = local_mask & local_wstrb;
-
-          mem_t muxed = 0;
-          for (int i = 0; i < sizeof(mem_t); i++) {
-            mem_t sel;
-            if (((be >> i) & 1) == 1) {
-              sel = (input >> (i * 8)) & 0xff;
-            } else {
-              sel = (base >> (i * 8)) & 0xff;
-            }
-            muxed |= (sel << (i * 8));
-          }
-
-          memory[addr] = muxed;
-          // printf("mem[%08x] = %09x\n", addr, muxed);
-        }
-      }
 
       uint64_t input = wdata.get_ui();
       if (pending_write_addr == 0x10001000) {
