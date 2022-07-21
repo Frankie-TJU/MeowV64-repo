54a1ca1dd8be48ef5cfb2146e7fc995a0f325629
Jiajie Chen <c@jia.je>
2022-07-21 11:37:10 +0800

    Improve verilator fix script

--- verilator/SingleCoreConfig/substitute.sh
+++ verilator/SingleCoreConfig/substitute.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# skip failedWrite check
-sed -i 's/for (i = 0;/\/\/\0/g' sram_*.v
-sed -i "s/mem\[i\] = {MEM_WIDTH{1'bx}};/\/\/\0/g" sram_*.v
+for file in sram_*.v; do
+	# fix QA_update
+	echo "\`define ARM_UD_MODEL" | cat - $file | sponge $file
+	# skip failedWrite check
+	sed -i 's/for (i = 0;/\/\/\0/g' $file
+	sed -i "s/mem\[i\] = {MEM_WIDTH{1'bx}};/\/\/\0/g" $file
+	# fix XQA
+	sed -i "s/XQA = 1'b1/XQA = 1'b0/g" $file
+done
