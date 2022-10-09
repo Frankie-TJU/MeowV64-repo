20547cc217e18b7075d9e8f0f3523c254dcd71da
Jiajie Chen <c@jia.je>
2022-10-09 15:13:22 +0800

    Fix compilation

--- riscof/spike/riscof_spike.py
+++ riscof/spike/riscof_spike.py
@@ -75,7 +75,7 @@ class spike(pluginTemplate):
        # Note the march is not hardwired here, because it will change for each
        # test. Similarly the output elf name and compile macros will be assigned later in the
        # runTests function
-       self.compile_cmd = 'riscv{1}-unknown-elf-gcc -march={0} \
+       self.compile_cmd = 'riscv{1}-unknown-elf-gcc -march={0}_zicsr_zifencei \
          -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g\
          -T '+self.pluginpath+'/env/link.ld\
          -I '+self.pluginpath+'/env/\
