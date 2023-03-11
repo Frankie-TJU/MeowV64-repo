76a2fd24ccac0ee2de3b1ae284c41b6ab62a8d72
Jiajie Chen <c@jia.je>
2023-03-11 15:45:08 +0800

    Bump riscv-test, skip ma_data test

--- verilator/common/test.sh
+++ verilator/common/test.sh
@@ -3,5 +3,7 @@ set -e
 
 make
 for filename in ../../testcases/meow/bin/*.bin ../../testcases/riscv-tests/build/isa/*.bin; do
-	./VRiscVSystem $filename
+	if [[ "$filename" != *"ma_data"* ]]; then
+		./VRiscVSystem $filename
+	fi
 done
--- verilator/difftest/test.sh
+++ verilator/difftest/test.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH=~/prefix/riscv-isa-sim/lib/
 
 make
 for filename in ../../testcases/riscv-tests/build/isa/*.bin; do
-	if [[ "$filename" != *"breakpoint"* ]] && [[ "$filename" != *"ma_fetch"* ]] && [[ "$filename" != *"dirty"* ]];then
+	if [[ "$filename" != *"breakpoint"* ]] && [[ "$filename" != *"ma_fetch"* ]] && [[ "$filename" != *"ma_data"* ]] && [[ "$filename" != *"dirty"* ]];then
 		./VRiscVSystem $filename
 	fi
 done
