2d6821a36d23e9f45ff4789f57d52fea31d945ff
Jiajie Chen <c@jia.je>
2024-10-29 14:27:15 +0800

    Add initial gauss seidel

---
+++ testcases/rvv/src/gauss_seidel.c
@@ -0,0 +1,58 @@
+#include "common.h"
+
+const int N = 100;
+const float err = 1e-3;
+float matrix[N][N + 1];
+float y[N];
+
+// https://www.javatpoint.com/gauss-seidel-method-in-c
+int main() {
+  int count, t, limit;
+  float temp, error, a, sum = 0;
+
+  printf_("Initialize data\r\n");
+  for (count = 0; count < N; count++) {
+    for (t = 0; t < N + 1; t++) {
+      matrix[count][t] = 1.0;
+    }
+  }
+
+  for (count = 0; count < N; count++) {
+    y[count] = 0;
+  }
+
+  do {
+    a = 0;
+
+    for (count = 0; count < N; count++) {
+      sum = 0;
+
+      for (t = 0; t < N; t++) // Removed extra condition 't a'
+      {
+        if (t != count) {
+          sum += matrix[count][t] * y[t];
+        }
+      }
+
+      temp = (matrix[count][N] - sum) / matrix[count][count];
+      error = temp - y[count];
+      if (error < 0) {
+        error = -error;
+      }
+
+      if (error > a) {
+        a = error;
+      }
+
+      y[count] = temp;
+    }
+  } while (a >= err);
+
+  printf_("\n\nSolution:\n\n");
+
+  for (count = 0; count < N; count++) {
+    printf_("Y[%d]:\t%f\n", count, y[count]);
+  }
+
+  return 0;
+}
