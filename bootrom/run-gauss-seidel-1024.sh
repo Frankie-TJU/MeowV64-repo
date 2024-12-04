31805b46910fef6aaf668e9ae396c33a55ef1fb9
Jiajie Chen <c@jia.je>
2024-12-04 19:39:14 +0800

    Add run scripts for fpga tests

---
+++ bootrom/run-gauss-seidel-1024.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 boot.py ../testcases/rvv/bin/sparse_gauss_seidel_vector_1024.bin /dev/ttyUSB2
---
+++ bootrom/run-gauss-seidel-512.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 boot.py ../testcases/rvv/bin/sparse_gauss_seidel_vector_512.bin /dev/ttyUSB2
